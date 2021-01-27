719e3cf39ff0f43370d9fab522188ab3e398da8a
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-01-27 16:51:18 +0300

    EPMDJ-5958: Update textarea
    Issue: EPMDJ-5958

--- src/forms/inputs/textarea/textarea-types.ts
+++ src/forms/inputs/textarea/textarea-types.ts
@@ -1,7 +1,7 @@
 export interface TextareaProps {
   className?: string;
   value: string;
-  onChange: (event: React.SyntheticEvent<HTMLTextAreaElement>) => void;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   disabled?: boolean;
   error?: boolean;
 }
--- src/forms/inputs/textarea/textarea.tsx
+++ src/forms/inputs/textarea/textarea.tsx
@@ -1,11 +1,19 @@
-import { BEM } from '@redneckz/react-bem-helper';
+import { BEM, textarea as bemTextarea } from '@redneckz/react-bem-helper';
 
 import { TextareaProps } from './textarea-types';
+import { INPUT_PROPS } from '../input/input-props';
 
 import styles from './textarea.module.scss';
 
 const textarea = BEM(styles);
 
-export const Textarea = textarea(({ className, ...rest }: TextareaProps) => (
-  <textarea className={className} {...rest} />
-));
+const INPUT_PROPS_OBJ = Object.assign({}, ...INPUT_PROPS.map((key) => ({ [key]: undefined })));
+
+export const Textarea = (props: TextareaProps) => <TextareaElement {...props} />;
+
+const TextareaElement = textarea.input(
+  bemTextarea({
+    ...INPUT_PROPS_OBJ,
+    value: '',
+  } as {}),
+);
