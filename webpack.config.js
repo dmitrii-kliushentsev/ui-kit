7bd11b4954d977aacb4ebca0f3c8fc8e841b54db
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-07-14 13:04:13 +0300

    build: add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, options) => {
+  return {
+    entry: path.resolve(__dirname, 'src/index.ts'),
+
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.js',
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    target: 'node',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-typescript', '@babel/preset-env'],
+              plugins: ['babel-plugin-styled-components'],
+            },
+          },
+          exclude: /(node_modules|bower_components)/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: (options.mode == 'production' ? true : false),
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [require('autoprefixer')],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images/',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+};
