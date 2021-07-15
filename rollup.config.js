c74308515ec987fe0278fa7d9e52199920796fb7
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-07-15 14:20:44 +0300

    build: added react traspilation to jsx/runtime

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
-import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
 import pkg from './package.json';
 
 export default {
@@ -24,15 +21,6 @@ export default {
       include: ['src/**/*'],
       exclude: 'node_modules/**',
     }),
-    // nodeResolve(),
-    // commonjs(),
-    // typescript({
-    //   typescript: require('typescript'),
-    // }),
-    // postcss({
-    //   modules: true,
-    // }),
-    // terser(),
-    // json(),
+    terser(),
   ],
 };
