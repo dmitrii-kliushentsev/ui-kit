be4e58b650d89a3598cd5cbaee10cab2abfef314
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-07-15 15:54:01 +0300

    build: remove unnecessary dependency

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -1,9 +1,6 @@
-const { resolve } = require('path');
-
 module.exports = {
   twin: {
     preset: 'styled-components',
-    config: "tailwind.config.js",
     autoCssProp: true,
     hasSuggestions: true,
     includeClassNames: true,
