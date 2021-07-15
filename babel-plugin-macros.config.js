1787929f6152ce5b0beb947261b6ff4715e638a5
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-07-15 16:07:05 +0300

    Update build configuration (#113)
    * build: add webpack.config
    
    * WIP
    - having webpack is not really necessary
    - add "&& tsc" to build script to generate types info
    - enable terser after issues fixed
    ISSUE: React import is not working properly
    
    * build: added react traspilation to jsx/runtime
    
    * build: remove unnecessary dependency
    
    * build: update package dependency
    
    * test: remove github check
    
    Co-authored-by: RomanDavlyatshin <davlyatshinra@gmail.com>

--- babel-plugin-macros.config.js
+++ babel-plugin-macros.config.js
@@ -1,5 +1,3 @@
-const { resolve } = require('path');
-
 module.exports = {
   twin: {
     preset: 'styled-components',
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
+import babel from 'rollup-plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
-import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
 import pkg from './package.json';
 
 export default {
@@ -16,15 +14,13 @@ export default {
   ],
   external: [...Object.keys(pkg.dependencies || {})],
   plugins: [
-    nodeResolve(),
+    nodeResolve({ extensions: ['.ts', '.tsx'] }),
     commonjs(),
-    typescript({
-      typescript: require('typescript'),
-    }),
-    postcss({
-      modules: true,
+    babel({
+      extensions: ['.ts', '.tsx'],
+      include: ['src/**/*'],
+      exclude: 'node_modules/**',
     }),
     terser(),
-    json(),
   ],
 };
