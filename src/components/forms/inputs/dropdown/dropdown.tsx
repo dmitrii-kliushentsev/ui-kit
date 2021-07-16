fe1d197d538af45a98ae3bf5e4654ce7b28be82a
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-07-16 16:15:27 +0300

    fix: incorrect data-test attribute

--- src/components/forms/inputs/dropdown/dropdown.tsx
+++ src/components/forms/inputs/dropdown/dropdown.tsx
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useRef, useState } from 'react';
 import 'twin.macro'
 
-import { Icons } from '../../../icon';
+import { Icons } from '../../../icon/index';
 import { useClickOutside } from '../../../../hooks';
 
 type Position = 'top' | 'bottom';
@@ -48,7 +48,7 @@ export const Dropdown = ({ items, action, selectedValue, className }: Props) => 
 
   return (
     <div ref={node} tw="relative flex items-center gap-x-1 text-monochrome-black cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-        <span ref={labelNode} tw="text-14 leading-20 font-bold" className={className} data-test="table-pagination:page-rows">
+        <span ref={labelNode} tw="text-14 leading-20 font-bold" className={className} data-test="dropdown:selected-value">
           {selectedValue}
         </span>
       <Icons.Expander width={8} height={8} rotate={isOpen ? -90 : 90} />
@@ -57,7 +57,7 @@ export const Dropdown = ({ items, action, selectedValue, className }: Props) => 
           top: position === 'bottom' ? `${dropdownLabelHeight + listMargin}px` : `-${dropdownHeight + listMargin}px`
         }}>
           {items.map(({label, value}) => (
-            <div tw="flex items-center pl-2 pr-8 text-14 leading-32 hover:bg-monochrome-light-tint" onClick={(() => action(value))} key={value}>
+            <div tw="flex items-center pl-2 pr-8 text-14 leading-32 hover:bg-monochrome-light-tint" data-test="dropdown:item" onClick={(() => action(value))} key={value}>
               {selectedValue === value && <Icons.Check width={14} height={10} viewBox="0 0 14 10" tw="absolute text-blue-default" />}
               <span tw="ml-6">{label}</span>
             </div>
