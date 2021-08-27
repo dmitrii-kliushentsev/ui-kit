3465fe65322552ea6ff8d907c56ecfb5e6eec691
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-08-27 13:11:09 +0300

    test: remove unnecessary test for modal component

--- src/components/modal/modal.spec.tsx
+++ src/components/modal/modal.spec.tsx
@@ -28,15 +28,6 @@ describe('Modal', () => {
     expect(screen.queryByText('children')).toBe(null);
   });
 
-  it('should call "onToggle" function with false value as a first argument', () => {
-    const { container } = render(<Modal onToggle={onToggle} isOpen>{children}</Modal>, {
-      container: document.body.appendChild(portalRootElement),
-    });
-    fireEvent.click(getByDataTest(container, 'modal:close-button'));
-    expect(onToggle).toBeCalledTimes(1);
-    expect(onToggle.mock.calls[0][0]).toBe(false);
-  });
-
   it('should call "onToggle" function after the close button is clicked with false value as first argument', () => {
     const { container } = render(<Modal onToggle={onToggle} isOpen>{children}</Modal>, {
       container: document.body.appendChild(portalRootElement),
@@ -46,7 +37,8 @@ describe('Modal', () => {
     expect(onToggle.mock.calls[0][0]).toBe(false);
   });
 
-  it('should call "onToggle" function after the fade block is clicked with false value as first argument if "isDisableFadeClick" is equal false', () => {
+  it('should call "onToggle" function with false value as first argument after the fade block is clicked ' +
+      'and if "isDisableFadeClick" is equal false', () => {
     const { container } = render(<Modal onToggle={onToggle} isOpen isDisableFadeClick={false}>{children}</Modal>, {
       container: document.body.appendChild(portalRootElement),
     });
@@ -55,7 +47,7 @@ describe('Modal', () => {
     expect(onToggle.mock.calls[0][0]).toBe(false);
   });
 
-  it('should not call "onToggle" function after the fade block is clicked if "isDisableFadeClick" is equal true', () => {
+  it('should not call "onToggle" function after the fade block is clicked and if "isDisableFadeClick" is equal true', () => {
     const { container } = render(<Modal onToggle={onToggle} isOpen isDisableFadeClick>{children}</Modal>, {
       container: document.body.appendChild(portalRootElement),
     });
