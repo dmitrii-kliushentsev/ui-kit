877f34eaab3ee7b12d0f8baf36e1a96fa7c88466
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-10-14 13:22:25 +0300

    refactor: disabled form group props

--- src/components/forms/disabled-form-group/disabled-form-group.tsx
+++ src/components/forms/disabled-form-group/disabled-form-group.tsx
@@ -3,7 +3,7 @@ import { Field } from 'formik';
 
 interface Field {
   label: React.ReactNode;
-  value: string;
+  name: string;
 }
 
 interface Props {
@@ -12,11 +12,11 @@ interface Props {
 
 export const DisabledFormGroup = ({ fields }: Props) => (
   <div tw="space-y-6 p-6 border border-monochrome-dark rounded leading-24 text-12">
-    {fields.map(({ label, value }) => (
+    {fields.map(({ label, name }) => (
       <div>
         <div tw="text-monochrome-dark-tint font-bold">{label}</div>
-        <Field name={value}>
-          {({ field }: any) => <div tw="text-14 text-monochrome-light-tint">{field?.value}</div>}
+        <Field name={name}>
+          {({ field }: any) => <div tw="text-14 text-monochrome-light-tint">{field?.name}</div>}
         </Field>
       </div>
     ))}
