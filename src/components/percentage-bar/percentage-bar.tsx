a7358c187dc6fbd539669390f38f0343350f769a
RomanAchilov <dakaktak95@gmail.com>
2021-12-09 00:18:54 +0300

    feat: update cells & add coverage progress cell

--- src/components/percentage-bar/percentage-bar.tsx
+++ src/components/percentage-bar/percentage-bar.tsx
@@ -6,15 +6,10 @@ interface Props {
 
 export const PercentageBar = ({ percentage }: Props) => (
   <div tw="w-25 h-1 bg-data-visualization-scrollbar-thumb bg-opacity-30 rounded">
-    <Progress style={{ width: `${percentage}%` }} percentage={percentage} />
+    <Progress style={{ width: `${percentage}%` }} />
   </div>
 );
 
 const Progress = styled.div`
-    ${tw`h-1 rounded`};
-
-    ${({ percentage }: Props) => [
-    percentage === 100 && tw`bg-data-visualization-scrollbar-thumb`,
-    percentage > 0 && percentage < 100 && tw`bg-data-visualization-coverage`,
-  ]}
+    ${tw`h-1 rounded bg-data-visualization-coverage`};
 `;
--- src/components/table/cells/coverage-progress-cell/coverage-progress-cell.tsx
+++ src/components/table/cells/coverage-progress-cell/coverage-progress-cell.tsx
@@ -13,27 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Icons } from '../../../icon';
 import 'twin.macro';
 
 import { percentFormatter } from '../../../../utils';
 import { ClickableCell } from '../clickable-cell';
+import { PercentageBar } from '../../../percentage-bar';
 
 interface Props {
   value: number;
 }
 
-export const CoverageCell = ({ value = 0 }: Props) => (
-  <div tw="leading-64">
+export const CoverageProgressCell = ({ value = 0 }: Props) => (
+  <div tw="flex items-center gap-x-2 leading-64">
+    <PercentageBar percentage={percentFormatter(value)} />
     <ClickableCell disabled>
-      {value === 0 && (
-        <span
-          tw="flex items-center mr-2 text-orange-default pointer-events-auto"
-          title="Test didn't cover any methods. Make sure the test is actual or modify/delete it."
-        >
-          <Icons.UncoveredMethods />
-        </span>
-      )}
       {percentFormatter(value)}
     </ClickableCell>
   </div>
