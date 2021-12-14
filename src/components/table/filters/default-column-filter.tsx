5aecea3e46f7afd5dccee1054748579996460cc0
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-14 10:17:56 +0300

    fix: add debounce

--- src/components/table/filters/default-column-filter.tsx
+++ src/components/table/filters/default-column-filter.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from 'react';
+import debounce from 'lodash.debounce';
 import { Inputs } from '../../forms';
 
 export function DefaultColumnFilter({
@@ -5,11 +7,22 @@ export function DefaultColumnFilter({
     filterValue = '', setFilter = () => {}, Header = '',
   } = {},
 }: any) {
+  const [value, setValue] = useState(filterValue);
+
+  const search = useCallback(debounce((v) => {
+    setFilter(v?.trimEnd() || v);
+  }, 300), [setFilter]);
+
+  const onChangeHandler = (v: string) => {
+    search(v);
+    setValue(v);
+  }
+
   return (
     <Inputs.Search
-      value={filterValue}
+      value={value}
       onChange={e => {
-        setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        onChangeHandler(e.target.value);
       }}
       reset={() => setFilter('')}
       placeholder={`Search by ${Header.toLowerCase()}`}
