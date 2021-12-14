46c801ede2c33fea7d11d9cdc7de650184742306
dmitrii-kliushentsev <94057436+dmitrii-kliushentsev@users.noreply.github.com>
2021-12-14 14:25:02 +0300

    EPMDJ-9375: Add debounce for default search column (#176)
    * fix: add debounce
    
    * refactor: default filter column
    
    * feat: bump version

--- src/components/table/filters/default-column-filter.tsx
+++ src/components/table/filters/default-column-filter.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useState } from 'react';
+import debounce from 'lodash.debounce';
 import { Inputs } from '../../forms';
 
 export function DefaultColumnFilter({
@@ -5,11 +7,22 @@ export function DefaultColumnFilter({
     filterValue = '', setFilter = () => {}, Header = '',
   } = {},
 }: any) {
+  const [valueInput, setValueInput] = useState(filterValue);
+
+  const search = useCallback(debounce((value) => {
+    setFilter(value);
+  }, 300), [setFilter]);
+
+  const onChangeHandler = (value: string) => {
+    search(value);
+    setValueInput(value);
+  }
+
   return (
     <Inputs.Search
-      value={filterValue}
+      value={valueInput}
       onChange={e => {
-        setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        onChangeHandler(e.target.value);
       }}
       reset={() => setFilter('')}
       placeholder={`Search by ${Header.toLowerCase()}`}
