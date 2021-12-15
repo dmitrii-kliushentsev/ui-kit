8cedc12b74d889328a157e3c6f1a44a13a5bb79d
ilia_solovev1 <ilia_solovev1@epam.com>
2021-12-15 12:14:53 +0300

    fix: decrease tailwind version to 2.2.6

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ const plugin = require('tailwindcss/plugin');
 const svgToDataUri = require('mini-svg-data-uri');
 
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false,
+  mode: 'jit',
   theme: {
     boxShadow: {
       DEFAULT: '0 0 24px rgba(0, 0, 0, 0.15)',
@@ -206,6 +208,12 @@ module.exports = {
       ...theme('colors'),
       'current-color': 'currentColor',
     }),
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+    },
   },
   plugins: [
     plugin(({ addUtilities }) => {
