8b2dc148a81dfca04d611a637fc26984417a92a2
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-19 19:35:05 +0300

    refactor: refactor portal component

--- src/components/portal/portal.tsx
+++ src/components/portal/portal.tsx
@@ -1,31 +1,28 @@
-import { Component } from 'react';
+import {
+  ReactChild, ReactPortal, useEffect, useMemo,
+} from 'react';
 import { createPortal } from 'react-dom';
 
-export class Portal<Props extends { rootElementId: string; displayContent: boolean; }> extends Component<Props> {
-  public element: HTMLDivElement;
-
-  public rootElementById: HTMLElement | null;
-
-  constructor(props: Props) {
-    super(props);
-    this.element = document.createElement('div');
-    this.rootElementById = document.getElementById(props.rootElementId);
-  }
-
-  public componentDidMount() {
-    if (this.rootElementById) {
-      this.rootElementById.appendChild(this.element);
-    }
-  }
-
-  public componentWillUnmount() {
-    if (this.rootElementById) {
-      this.rootElementById.removeChild(this.element);
-    }
-  }
-
-  public render() {
-    const { children, displayContent } = this.props;
-    return displayContent ? createPortal(children, this.element) : null;
-  }
+interface Props {
+  rootElementId?: string,
+  displayContent: boolean,
+  children: ReactChild,
 }
+
+export const Portal = ({ rootElementId = '', displayContent, children }: Props): ReactPortal | null => {
+  const element = useMemo(() => document.createElement('div'), [rootElementId]);
+  const portal = useMemo(() => createPortal(children, element), [element, children]);
+  useEffect(() => {
+    const rootElementById = document.getElementById(rootElementId);
+    if (rootElementById) {
+      rootElementById.appendChild(element);
+    }
+    return () => {
+      if (rootElementById) {
+        rootElementById.removeChild(element);
+      }
+    };
+  }, [rootElementId]);
+
+  return displayContent ? portal : null;
+};
