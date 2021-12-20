80719c32e384c833ee5d58259f8ca23526382329
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-20 09:55:52 +0300

    feat: implement label component

---
+++ src/components/label/label.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import tw, { styled } from 'twin.macro';
+import { Icons } from '../icon';
+
+interface Props {
+  children: React.ReactNode,
+  deleteHandler?: () => void,
+}
+
+const DeleteButton = styled.div`
+  ${tw`flex items-center justify-center h-4 w-4 rounded-full hover:bg-monochrome-dark-tint`}
+`;
+
+export const Label = ({ children, deleteHandler }: Props) => (
+  <div tw="flex gap-1 items-center w-min px-2 bg-monochrome-medium-tint rounded-sm">
+    <span tw="leading-24 text-14 text-monochrome-default">{children}</span>
+    {deleteHandler ? <DeleteButton onClick={deleteHandler}><Icons.Close width={8} height={8} /></DeleteButton> : null}
+  </div>
+);
