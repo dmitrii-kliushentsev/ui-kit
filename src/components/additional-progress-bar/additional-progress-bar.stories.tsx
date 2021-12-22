f77f1996d101d9ed6ff977e48f1cfe7c8835ad6b
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-23 01:20:06 +0300

    feat: implements new stories for add. progress bar

--- src/components/additional-progress-bar/additional-progress-bar.stories.tsx
+++ src/components/additional-progress-bar/additional-progress-bar.stories.tsx
@@ -1,23 +1,35 @@
-import { ChangeEvent, useState } from 'react';
-import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
 import 'twin.macro';
 
 import { AdditionalProgressBar } from './additional-progress-bar';
 
-storiesOf('AdditionalProgressBar', module).add(
-  'AdditionalProgressBar with change coverage panel',
-  () => {
-    const [coverage, setCoverage] = useState(0);
-    const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-      setCoverage(Number(value));
-    };
-    return (
-      <div tw="flex flex-col gap-5 p-5">
-        <AdditionalProgressBar value={`${coverage}%`} />
-        <AdditionalProgressBar value={`${coverage}%`} type="primary" />
-        <AdditionalProgressBar value={`${coverage}%`} type="secondary" />
-        <input type="range" min="0" max="100" value={coverage} onChange={handleChange} />
-      </div>
-    );
+export default {
+  title: 'AdditionalProgressBar',
+  component: AdditionalProgressBar,
+  argTypes: {
+    value: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 300,
+        step: 1,
+      },
+    },
+    type: {
+      control: {
+        type: 'radio',
+      },
+      options: ['primary', 'secondary'],
+    },
   },
-);
+} as ComponentMeta<typeof AdditionalProgressBar>;
+
+const Template = (args) => <AdditionalProgressBar value={100} {...args} />;
+export const Default = Template.bind({});
+
+export const Primary = Template.bind({});
+Primary.args = { type: 'primary' };
+
+export const Secondary = Template.bind({});
+Secondary.args = { type: 'secondary' };
