1fd78f595f1e8ca7922c08f6e199614033bb6d12
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-23 04:00:49 +0300

    feat: implements badge component stories

--- src/components/badge/badge.stories.tsx
+++ src/components/badge/badge.stories.tsx
@@ -1,16 +1,72 @@
-import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
 import 'twin.macro';
 
 import { Badge } from './badge';
 
-storiesOf('Badge', module)
-  .add('with simple and bold text', () => (
-    <div tw="flex justify-between w-full">
-      <Badge>simple</Badge>
-      <Badge bold>bold</Badge>
-      <Badge color="green">Online</Badge>
-      <Badge color="green" bold>NEW</Badge>
-      <Badge color="orange">Busy</Badge>
-      <Badge color="red">Offline</Badge>
-    </div>
-  ));
+export default {
+  title: 'Badge',
+  component: Badge,
+  argTypes: {
+    color: {
+      control: { type: 'radio' },
+      options: ['gray', 'green', 'orange', 'red'],
+    },
+    bold: {
+      control: 'boolean',
+    },
+    children: {
+      control: 'text',
+    },
+  },
+} as ComponentMeta<typeof Badge>;
+
+const Template = (args) => <Badge children="Badge" {...args} />;
+export const Default = Template.bind({});
+Default.parameters = {
+  docs: {
+    source: {
+      code: '<Badge>Badge</Badge>',
+    },
+  },
+};
+
+export const Gray = Template.bind({});
+Gray.args = { color: 'gray' };
+Gray.parameters = {
+  docs: {
+    source: {
+      code: '<Badge color="gray">Badge</Badge>',
+    },
+  },
+};
+
+export const Green = Template.bind({});
+Green.args = { color: 'green' };
+Green.parameters = {
+  docs: {
+    source: {
+      code: '<Badge color="green">Badge</Badge>',
+    },
+  },
+};
+
+export const Orange = Template.bind({});
+Orange.args = { color: 'orange' };
+Orange.parameters = {
+  docs: {
+    source: {
+      code: '<Badge color="orange">Badge</Badge>',
+    },
+  },
+};
+
+export const Red = Template.bind({});
+Red.args = { color: 'red' };
+Red.parameters = {
+  docs: {
+    source: {
+      code: '<Badge color="red">Badge</Badge>',
+    },
+  },
+};
