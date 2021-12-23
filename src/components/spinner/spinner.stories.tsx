95b926b3a28325de85020e40ad52b09099687ae1
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-23 14:33:32 +0300

    feat: implements stories for spenner component

--- src/components/spinner/spinner.stories.tsx
+++ src/components/spinner/spinner.stories.tsx
@@ -1,8 +1,16 @@
-import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
+
 import { Spinner } from './spinner';
 
-storiesOf('Spinner', module).add('Spinner', () => (
-  <div>
-    <Spinner />
-  </div>
-));
+export default {
+  title: 'Spinner',
+  component: Spinner,
+} as ComponentMeta<typeof Spinner>;
+
+const Template = (args) => <Spinner {...args} />;
+export const Default = Template.bind({});
+
+export const Disabled = Template.bind({});
+Disabled.args = { disabled: true };
