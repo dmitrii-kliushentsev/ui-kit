2ac58e94740d3fbd31e41afff5c5b05511562ac5
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-23 18:29:05 +0300

    feat: implements stories for tab component

--- src/components/tab/tab.stories.tsx
+++ src/components/tab/tab.stories.tsx
@@ -1,10 +1,31 @@
-import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
 
 import { Tab } from './tab';
 
-storiesOf('Tabs', module).add('Tabs', () => (
-  <div>
-    <Tab active>General</Tab>
-    <Tab name="system">System</Tab>
-  </div>
-));
+export default {
+  title: 'Tab',
+  component: Tab,
+} as ComponentMeta<typeof Tab>;
+
+const Template = (args) => <Tab {...args}>Tab</Tab>;
+export const Default = Template.bind({});
+Default.args = { active: false };
+Default.parameters = {
+  docs: {
+    source: {
+      code: '<Tab>Tab</Tab>',
+    },
+  },
+};
+
+export const Active = Template.bind({});
+Active.args = { active: true };
+Active.parameters = {
+  docs: {
+    source: {
+      code: '<Tab active>Tab</Tab>',
+    },
+  },
+};
