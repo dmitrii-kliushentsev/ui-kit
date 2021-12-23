73b5c3f3ebb7bb8c9a82777217b4c9179da1222f
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-23 05:09:55 +0300

    feat: implements stories for menu component

--- src/components/menu/menu.stories.tsx
+++ src/components/menu/menu.stories.tsx
@@ -1,60 +1,61 @@
-import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import 'twin.macro';
 
 import { Menu } from './menu';
 
-storiesOf('Menu', module).add('Menu', () => (
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
-    <Menu
-      items={[
-        {
-          label: 'Rename',
-          icon: 'Edit',
-          onClick: () => {},
-        },
-        {
-          label: 'Settings',
-          icon: 'Settings',
-          onClick: () => {},
-        },
-        {
-          label: 'Agents list',
-          icon: 'Agents',
-          onClick: () => {},
-        },
-        {
-          label: 'Logs',
-          icon: 'Logs',
-          onClick: () => {},
-        },
-      ]}
-    />
-  </div>
-)).add('Menu with border', () => (
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
-    <Menu
-      bordered
-      items={[
-        {
-          label: 'Rename',
-          icon: 'Edit',
-          onClick: () => {},
-        },
-        {
-          label: 'Settings',
-          icon: 'Settings',
-          onClick: () => {},
-        },
-        {
-          label: 'Agents list',
-          icon: 'Agents',
-          onClick: () => {},
-        },
-        {
-          label: 'Logs',
-          icon: 'Logs',
-          onClick: () => {},
-        },
-      ]}
-    />
-  </div>
-));
+const menuItems = [
+  {
+    label: 'Rename',
+    icon: 'Edit',
+    onClick: () => {},
+  },
+  {
+    label: 'Settings',
+    icon: 'Settings',
+    onClick: () => {},
+  },
+  {
+    label: 'Agents list',
+    icon: 'Agents',
+    onClick: () => {},
+  },
+  {
+    label: 'Logs',
+    icon: 'Logs',
+    onClick: () => {},
+  },
+];
+
+export default {
+  title: 'Menu',
+  component: Menu,
+  decorators: [
+    (Story) => (
+      <div style={{ marginLeft: '500px', padding: '300px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Menu>;
+
+const Template = (args) => <Menu items={menuItems} {...args} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  docs: {
+    source: {
+      code: '<Menu items={menuItems} />',
+    },
+  },
+};
+
+export const WithBorder = Template.bind({});
+WithBorder.args = { bordered: true };
+WithBorder.parameters = {
+  docs: {
+    source: {
+      code: '<Menu bordered items={menuItems} />',
+    },
+  },
+};
