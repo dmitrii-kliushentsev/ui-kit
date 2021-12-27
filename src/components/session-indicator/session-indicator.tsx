d1363ee1c335cc07214f4d3b29b9dad9f19dd6e3
Ilia_Solovev1 <49729985+IliaSolovev@users.noreply.github.com>
2021-12-27 12:02:48 +0300

    EPMDJ-9460: Fix session indicator does not blink (#188)
    Co-authored-by: ilia_solovev1 <ilia_solovev1@epam.com>

--- src/components/session-indicator/session-indicator.tsx
+++ src/components/session-indicator/session-indicator.tsx
@@ -1,4 +1,4 @@
-import { css, styled } from 'twin.macro';
+import tw, { css, styled } from 'twin.macro';
 
 import { path as circlePath } from './circle.path';
 import { path as eyePath } from './eye.path';
@@ -12,7 +12,7 @@ export const SessionIndicator = ({ active }: Props) => (
     <g fillRule="nonzero" stroke="none" strokeWidth="1" fill="none">
       <CirclePath d={circlePath} />
       {active
-        ? <EyePath d={eyePath} active tw="animate-blinker" data-test="session-indicator:active" />
+        ? <EyePath d={eyePath} active data-test="session-indicator:active" />
         : <EyePath d={eyePath} data-test="session-indicator:inactive" />}
     </g>
   </Wrapper>
@@ -38,12 +38,25 @@ const CirclePath = styled.path`
   }
 `;
 
-const EyePath = styled.path<{active?: boolean}>`
+const EyePath = styled.path<{ active?: boolean }>`
   stroke: #00b602;
   stroke-width: 1.3;
-  
+
   ${({ active }) => active && css`
+    animation: blink 2s linear infinite;
     stroke: none;
     fill: #ee0000;
-  `}
+  `} 
+  
+  @keyframes blink {
+    0% {
+      ${tw`opacity-0`}
+    }
+    50% {
+      ${tw`opacity-100`}
+    }
+    100% {
+      ${tw`opacity-0`}
+    }
+  }
 `;
