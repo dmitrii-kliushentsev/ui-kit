d0e5b666802578caed16e125f1dc0e8232dcb609
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2021-12-28 12:36:28 +0300

    refactor: add type for tab story

--- src/components/tab/tab.stories.tsx
+++ src/components/tab/tab.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ComponentMeta } from '@storybook/react';
+import { ComponentMeta, Story } from '@storybook/react';
 import 'twin.macro';
 
 import { Tab } from './tab';
@@ -9,7 +9,7 @@ export default {
   component: Tab,
 } as ComponentMeta<typeof Tab>;
 
-const Template = (args) => <Tab {...args}>Tab</Tab>;
+const Template: Story = (args) => <Tab {...args}>Tab</Tab>;
 export const Default = Template.bind({});
 Default.args = { active: false };
 Default.parameters = {
