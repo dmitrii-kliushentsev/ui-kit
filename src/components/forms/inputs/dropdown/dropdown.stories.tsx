c70523f6cd34543789a4d40143b8aec423690fda
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-01-10 10:20:19 +0300

    feat: add items option for dropdown story

--- src/components/forms/inputs/dropdown/dropdown.stories.tsx
+++ src/components/forms/inputs/dropdown/dropdown.stories.tsx
@@ -9,24 +9,26 @@ export default {
   component: Dropdown,
 } as Meta;
 
-export const Template: Story = () => {
+export const Template: Story = (args) => {
+  const { items } = args;
   const [value, setValue] = useState<any>('first item');
   return (
     <div tw="pt-[100px]">
       <Dropdown
-        items={[
-          {
-            value: 'first item',
-            label: 'first item',
-          },
-          {
-            value: 'second item',
-            label: 'second item with custom label',
-          },
-        ]}
+        items={items}
         value={value}
         onChange={(newValue) => setValue(newValue)}
       />
     </div>
   );
 };
+Template.args = { items: [
+  {
+    value: 'first item',
+    label: 'first item',
+  },
+  {
+    value: 'second item',
+    label: 'second item with custom label',
+  },
+] }
