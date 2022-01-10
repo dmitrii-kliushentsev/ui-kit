6077fb56336702295f36863162a0b9747ddbb249
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-01-10 05:09:11 +0300

    feat: add parametrs for dark input stories

--- src/components/forms/inputs/dark-input/dark-input.stories.tsx
+++ src/components/forms/inputs/dark-input/dark-input.stories.tsx
@@ -7,6 +7,11 @@ import { DarkInput } from './dark-input';
 export default {
   title: 'DarkInput',
   component: DarkInput,
+  argTypes: {
+    touched: {type: 'boolean'},
+    error: {type: 'boolean'},
+    disabled: {type: 'boolean'},
+  }
 } as ComponentMeta<typeof DarkInput>;
 
 const Template: Story = (args) => <DarkInput {...args} />;
