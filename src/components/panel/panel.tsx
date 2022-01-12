643fdde6d65c34a21ae41abe401f382125a98bba
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-01-12 17:27:22 +0300

    refactor: refactor

--- src/components/panel/panel.tsx
+++ src/components/panel/panel.tsx
@@ -9,7 +9,7 @@ type State = {isOpen: boolean; setIsOpen: (value: boolean) => void};
 type Children = React.ReactChild | React.ReactChild[] | React.FC<State>;
 const defaultState: State = { isOpen: false, setIsOpen: () => {} };
 
-const PopupContext = createContext<State>(defaultState);
+const PanelContext = createContext<State>(defaultState);
 
 interface PanelProps {
   children: Children;
@@ -19,37 +19,36 @@ interface PanelProps {
 const Panel = ({
   children,
   isDisableFadeClick,
-  ...rest,
+  ...rest
 }: PanelProps) => {
   const [isOpen, setIsOpen] = useState(true);
   return (
-    <PopupContext.Provider value={{ isOpen, setIsOpen }}>
+    <PanelContext.Provider value={{ isOpen, setIsOpen }}>
       <Portal rootElementId="panel" displayContent={isOpen}>
         <div tw="flex w-full h-full justify-center items-center" {...rest}>
           {children}
-          <PanelFade onClick={() => !isDisableFadeClick && setIsOpen(false)} data-test="modal:fade" />
+          <PanelFade onClick={() => !isDisableFadeClick && setIsOpen(false)} data-test="panel:fade" />
         </div>
       </Portal>
-    </PopupContext.Provider>
+    </PanelContext.Provider>
   );
 };
 
 const PanelContent: FC = ({
   children,
-  ...rest,
+  ...rest
 }) => {
-  const { setIsOpen } = useContext(PopupContext);
+  const { isOpen, setIsOpen } = useContext(PanelContext);
   return (
     <div tw="w-97 absolute flex flex-col top-0 right-0 h-full z-[100] bg-monochrome-white shadow-[-4px 0 0 0 #007fff, -20px 0 40px 0 rgba(15, 36, 52, 0.15)]" {...rest}>
-      <CloseButtonPanel onClick={() => setIsOpen(false)} data-test="modal:close-button" />
-      {children}
+      <CloseButtonPanel onClick={() => setIsOpen(false)} data-test="panel:close-button" />
+      {typeof children === 'function' ? children({ setIsOpen, isOpen }) : children}
     </div>
   );
 };
 
 interface PanelCloseButtonProps {
   onClick: () => void;
-  'data-test': string;
 }
 
 const CloseButtonPanel:FC<PanelCloseButtonProps> = (rest) => (
@@ -77,9 +76,7 @@ const PanelBody: FC = ({ children }) => (
 )
 
 const PanelFade = styled.div`
-  ${tw`absolute w-full h-full top-0 left-0 bg-monochrome-white`};
-  opacity: 0.75;
-  z-index: 90;
+  ${tw`absolute w-full h-full top-0 left-0 bg-monochrome-white opacity-75 z-[90]`};
 `;
 
 Panel.Content = PanelContent;
