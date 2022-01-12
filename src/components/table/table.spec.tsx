b9537980614ae2bc34d77a8dce568de167825fea
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-01-12 21:21:04 +0300

    fix: tests for table component

--- src/components/table/table.spec.tsx
+++ src/components/table/table.spec.tsx
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import {
   fireEvent, render, screen,
 } from '@testing-library/react';
 import { Table } from './table';
 import { TableActionsProvider } from './table-actions';
 import { getAllByDataTest, getByDataTest, queryByDataTest } from '../../../test-utils';
+import { Cells } from './cells';
+import { Icons } from '../icon/index';
+import { Skeleton } from '../skeleton';
+import { Stub } from '../stub';
 
 const createData = (length: number) => Array.from({ length }, (_, i) => ({
   buildVersion: `0.${i}.0`,
@@ -28,75 +33,74 @@ const createData = (length: number) => Array.from({ length }, (_, i) => ({
 }));
 
 const props = {
-  isDefaulToggleSortBy: true,
+  isLoading: false,
+  data: Array.from({ length: 60 }, (_, i) => ({
+    id: `[engine:junit-jupiter]/[class:api.standalone.StandaloneApiTest]/[method:junit5IgnoredTest()]:AUTO-${i}`,
+    type: 'AUTO',
+    name: `[engine:junit-jupiter]/[class:api.standalone.StandaloneApiTest]/[method:junit5IgnoredTest()]-${i}`,
+    coverage: ++i,
+  })),
   columns: [
     {
       Header: 'Name',
-      accessor: 'buildVersion',
-      Cell: ({ value: buildVersion }: any) => (
-        <div tw="grid gap-x-2 h-12 items-center font-bold text-14">
-          <div tw="link text-ellipsis">{buildVersion}</div>
-        </div>
-      ),
+      accessor: 'name',
       textAlign: 'left',
-      width: '30%',
+      filterable: true,
+      isCustomCell: true,
+      width: '100%',
+      Cell: ({ value = '', state }: any) => (value ? (
+        <Cells.Compound
+          cellName={value}
+          cellAdditionalInfo="foo-bar"
+          icon={<Icons.Function />}
+        >
+          <Cells.Highlight text={value} searchWords={state.filters.map((filter: {value: string}) => filter.value)} />
+        </Cells.Compound>
+      ) : <Skeleton withIcon withSubLine />),
     },
     {
-      Header: 'Added',
-      accessor: 'detectedAt',
+      Header: 'Type',
+      accessor: 'type',
       textAlign: 'left',
-      width: '20%',
+      width: '100%',
+      Cell: ({ value = '' }: any) => (value ? (
+        <Cells.Compound
+          cellName={value}
+          icon={<Icons.Class />}
+        >
+          {value}
+        </Cells.Compound>
+      ) : <Skeleton withIcon withSubLine />),
     },
     {
-      Header: 'Total methods',
-      accessor: 'summary.total',
-      width: '10%',
-    },
-    {
-      Header: 'New',
-      accessor: 'summary.new',
-      width: '10%',
-    },
-    {
-      Header: 'Modified',
-      accessor: 'summary.modified',
-      width: '10%',
-    },
-    {
-      Header: 'Unaffected',
-      accessor: 'summary.unaffected',
-      width: '10%',
-    },
-    {
-      Header: 'Deleted',
-      accessor: 'summary.deleted',
-      width: '10%',
+      Header: 'Coverage, %',
+      accessor: 'coverage',
+      textAlign: 'left',
+      width: '100%',
+      Cell: ({ value = '' }: any) => (value
+        ? <Cells.Coverage value={value} />
+        : <Skeleton />),
     },
   ],
-  data: createData(26),
+  withSearch: true,
+  isDefaulToggleSortBy: true,
+  placeholder: 'Search packages by name',
+  renderHeader: ({ currentCount, totalCount }: { currentCount: number, totalCount: number }) => (
+    <div tw="flex justify-between text-monochrome-default text-14 leading-24 pb-3">
+      <div tw="uppercase font-bold">{`Current (${currentCount})`}</div>
+      <div>{`Displaying ${currentCount} of ${totalCount}`}</div>
+    </div>
+  ),
 };
 
 describe('Table', () => {
-  it('should render table with search if props "withSearch" is equal true', () => {
-    render(<TableActionsProvider><Table {...props} withSearch placeholder="table placeholder" /></TableActionsProvider>);
-    expect(screen.getByPlaceholderText('table placeholder')).toBeInTheDocument();
-  });
-
-  it('should render table without search if props "withSearch" is equal false', () => {
-    render(<TableActionsProvider><Table {...props} withSearch={false} placeholder="table placeholder" /></TableActionsProvider>);
-    expect(screen.queryByPlaceholderText('table placeholder')).toBeNull();
-  });
-
-  it('should render table with stub if it passed via props', () => {
-    const { rerender } = render(<TableActionsProvider><Table {...props} stub={<div>stub</div>} /></TableActionsProvider>);
+  it('should render table with stub if no rows', () => {
+    const { rerender } = render(<BrowserRouter><TableActionsProvider><Table {...props} stub={<div>stub</div>} data={[]} /></TableActionsProvider></BrowserRouter>);
     expect(screen.getByText('stub')).toBeInTheDocument();
-
-    rerender(<TableActionsProvider><Table {...props} /></TableActionsProvider>);
-    expect(screen.queryByText('stub')).toBeNull();
   });
 
   it('should not render pagination if rows less than 25', () => {
-    const { container } = render(<TableActionsProvider><Table {...props} data={createData(24)} /></TableActionsProvider>);
+    const { container } = render(<BrowserRouter><TableActionsProvider><Table {...props} data={createData(24)} /></TableActionsProvider></BrowserRouter>);
     expect(queryByDataTest(container, 'table:displaying-results-count')).toBeNull();
     expect(queryByDataTest(container, 'table:rows:count')).toBeNull();
     expect(queryByDataTest(container, 'table-pagination:pagination-arrow-left')).toBeNull();
@@ -104,20 +108,22 @@ describe('Table', () => {
   });
 
   it('should render 25 rows by default data.length more than 25', () => {
-    render(<TableActionsProvider><Table {...props} data={createData(26)} /></TableActionsProvider>);
-    expect(screen.getAllByText('1625224274896').length).toBe(25);
+    const { container } = render(<BrowserRouter><TableActionsProvider><Table {...props} data={createData(26)} /></TableActionsProvider></BrowserRouter>);
+    expect(queryByDataTest(container, 'table-pagination:pagination-arrow-left')).toBeInTheDocument();
+    expect(queryByDataTest(container, 'table-pagination:pagination-arrow-right')).toBeInTheDocument();
   });
 
-  it('should render 50 rows if select it in select rows dropdown', () => {
-    const { container } = render(<TableActionsProvider><Table {...props} data={createData(51)} /></TableActionsProvider>);
+  it('should not render pagination if selected display 50 rows and rendered less that 50', () => {
+    const { container } = render(<BrowserRouter><TableActionsProvider><Table {...props} data={createData(50)} /></TableActionsProvider></BrowserRouter>);
     fireEvent.click(getByDataTest(container, 'table-pagination:select-rows-dropdown'));
     fireEvent.click(getAllByDataTest(container, 'table-pagination:select-rows-dropdown:item')[1]);
-    expect(screen.getAllByText('1625224274896').length).toBe(50);
+    expect(queryByDataTest(container, 'table-pagination:pagination-arrow-left')).toBeNull();
+    expect(queryByDataTest(container, 'table-pagination:pagination-arrow-right')).toBeNull();
   });
 
   it('should throw error when data is not an object and display stub', () => {
     // @ts-ignore
-    render(<TableActionsProvider><Table {...props} data="" /></TableActionsProvider>);
+    render(<BrowserRouter><TableActionsProvider><Table {...props} data="" /></TableActionsProvider></BrowserRouter>);
     expect(screen.getByText('Table load failed')).toBeInTheDocument();
   });
 });
