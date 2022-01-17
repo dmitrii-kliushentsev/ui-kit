4967b4a44afc30316d379d0e1200e1e15cf64ad6
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-01-17 13:01:29 +0300

    feat: write test for panel component

--- src/components/panel/panel.spec.tsx
+++ src/components/panel/panel.spec.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { Panel } from './panel';
 import { queryByDataTest } from '../../../test-utils';
-import { Button } from '../forms';
 
 const onToggle = jest.fn();
 const portalRootElement = document.createElement('div');
@@ -43,5 +42,37 @@ describe('Panel', () => {
     expect(queryByDataTest(container, 'panel:header')).toBeInTheDocument();
     expect(queryByDataTest(container, 'panel:body')).toBeInTheDocument();
     expect(queryByDataTest(container, 'panel:footer')).toBeInTheDocument();
+  });
+  it('should close panel and run onClose click close button', () => {
+    const { container } = render(<Template onClose={onToggle} />, {
+      container: document.body.appendChild(portalRootElement),
+    });
+    fireEvent(
+      queryByDataTest(container, 'panel:close-button') || document.body,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    );
+    expect(queryByDataTest(container, 'panel:header')).toBeNull();
+    expect(queryByDataTest(container, 'panel:body')).toBeNull();
+    expect(queryByDataTest(container, 'panel:footer')).toBeNull();
+    expect(onToggle).toHaveBeenCalledTimes(1);
+  });
+  it('should close panel and run onClose click fade', () => {
+    const { container } = render(<Template onClose={onToggle} />, {
+      container: document.body.appendChild(portalRootElement),
+    });
+    fireEvent(
+      queryByDataTest(container, 'panel:fade') || document.body,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    );
+    expect(queryByDataTest(container, 'panel:header')).toBeNull();
+    expect(queryByDataTest(container, 'panel:body')).toBeNull();
+    expect(queryByDataTest(container, 'panel:footer')).toBeNull();
+    expect(onToggle).toHaveBeenCalledTimes(1);
   });
 });
