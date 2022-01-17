d3509a854cfe2bea266f49ac5dfb057375c5fcac
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-01-17 04:44:41 +0300

    feat: write tests for modal

--- src/components/modal/modal.spec.tsx
+++ src/components/modal/modal.spec.tsx
@@ -1,59 +1,49 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import React, { FC } from 'react';
+import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import { Modal } from './modal';
-import { getByDataTest } from '../../../test-utils';
+import { Button } from '../forms';
+import { queryByDataTest } from '../../../test-utils';
 
 const onToggle = jest.fn();
-const HEADER = <div>HEADER</div>;
-const CHILDREN = <div>CHILDREN</div>;
 const portalRootElement = document.createElement('div');
 portalRootElement.setAttribute('id', 'modal');
+
+interface TemplateProps {
+  onClose?: () => void;
+  type?: 'error' | 'info';
+  closeOnFadeClick?: boolean;
+}
+
+const Template: FC<TemplateProps> = ({
+  children, type, onClose, closeOnFadeClick, ...rest
+}) => (
+  <Modal onClose={onClose}>
+    <Modal.Content type={type} closeOnFadeClick={closeOnFadeClick}>
+      <Modal.Header data-test="modal:header">Header</Modal.Header>
+      <Modal.Body data-test="modal:body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ullam praesentium laudantium delectus...</p>
+      </Modal.Body>
+      <Modal.Footer data-test="modal:footer">
+        <Button primary size="large">
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal.Content>
+  </Modal>
+);
 
 beforeEach(() => {
   onToggle.mockRestore();
 });
 
-describe('Popup', () => {
-  it('should render popup with children and header when has "isOpen=true" props', () => {
-    render(<Modal onToggle={onToggle} isOpen header={HEADER}>{CHILDREN}</Modal>, {
+describe('Modal', () => {
+  it('should render modal with header, body and footer', () => {
+    const { container } = render(<Template onClose={onToggle} />, {
       container: document.body.appendChild(portalRootElement),
     });
-    expect(screen.getByText('CHILDREN')).toBeInTheDocument();
-    expect(screen.getByText('HEADER')).toBeInTheDocument();
-  });
-
-  it('should not render popup with children and header when has "isOpen=false" props', () => {
-    render(<Modal onToggle={onToggle} header={HEADER} isOpen={false}>{CHILDREN}</Modal>, {
-      container: document.body.appendChild(portalRootElement),
-    });
-    expect(screen.queryByText('CHILDREN')).toBe(null);
-    expect(screen.queryByText('HEADER')).toBe(null);
-  });
-
-  it('should call "onToggle" function after the close button is clicked with false value as first argument', () => {
-    const { container } = render(<Modal header={HEADER} onToggle={onToggle} isOpen>{CHILDREN}</Modal>, {
-      container: document.body.appendChild(portalRootElement),
-    });
-    fireEvent.click(getByDataTest(container, 'popup:close-button'));
-    expect(onToggle).toBeCalledTimes(1);
-    expect(onToggle.mock.calls[0][0]).toBe(false);
-  });
-
-  it('should call "onToggle" function with false value as first argument and after the fade block is clicked ' +
-      'if "closeOnFadeClick" is equal true', () => {
-    const { container } = render(<Modal header={HEADER} onToggle={onToggle} isOpen closeOnFadeClick>{CHILDREN}</Modal>, {
-      container: document.body.appendChild(portalRootElement),
-    });
-    fireEvent.click(getByDataTest(container, 'popup:fade'));
-    expect(onToggle).toBeCalledTimes(1);
-    expect(onToggle.mock.calls[0][0]).toBe(false);
-  });
-
-  it('should not call "onToggle" function after the fade block is clicked and if "closeOnFadeClick" is equal false', () => {
-    const { container } = render(<Modal header={HEADER} onToggle={onToggle} isOpen closeOnFadeClick={false}>{CHILDREN}</Modal>, {
-      container: document.body.appendChild(portalRootElement),
-    });
-    fireEvent.click(getByDataTest(container, 'popup:fade'));
-    expect(onToggle).toBeCalledTimes(0);
+    expect(queryByDataTest(container, 'modal:header')).toBeInTheDocument();
+    expect(queryByDataTest(container, 'modal:body')).toBeInTheDocument();
+    expect(queryByDataTest(container, 'modal:footer')).toBeInTheDocument();
   });
 });
--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -8,20 +8,6 @@ import { Button } from '../forms';
 export default {
   title: 'Modal',
   component: Modal,
-  argTypes: {
-    type: {
-      control: {
-        type: 'radio',
-      },
-      options: ['info', 'error'],
-    },
-    closeOnFadeClick: {
-      control: {
-        type: 'boolean',
-        default: true,
-      },
-    },
-  },
 } as Meta;
 
 const Template = ({ closeOnFadeClick, type }: PanelProps) => (
@@ -32,22 +18,51 @@ const Template = ({ closeOnFadeClick, type }: PanelProps) => (
           <Button primary size="large" onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? 'Close' : 'Open'}
           </Button>
-          <Modal.Panel type={type} closeOnFadeClick={closeOnFadeClick}>
+          <Modal.Content type={type} closeOnFadeClick={closeOnFadeClick}>
             <Modal.Header>Header</Modal.Header>
-            <div tw="px-6">
+            <Modal.Body>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ullam praesentium laudantium delectus...</p>
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure magni quia excepturi aliquid quas nostrum?</p>
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas nisi corrupti dolorem incidunt quisquam.</p>
-            </div>
+            </Modal.Body>
             <Modal.Footer>
               <Button primary size="large" onClick={() => setIsOpen(false)}>
                 Close
               </Button>
             </Modal.Footer>
-          </Modal.Panel>
+          </Modal.Content>
         </>
       )}
     </Modal>
   </div>
 );
 export const Default = Template.bind({});
+Default.parameters = {
+  docs: {
+    source: {
+      code: '<Modal>\n' +
+        '      {({ setIsOpen, isOpen }) => (\n' +
+        '        <>\n' +
+        '          <Button primary size="large" onClick={() => setIsOpen(!isOpen)}>\n' +
+        '            {isOpen ? \'Close\' : \'Open\'}\n' +
+        '          </Button>\n' +
+        '          <Modal.Content type={type} closeOnFadeClick={closeOnFadeClick}>\n' +
+        '            <Modal.Header>Header</Modal.Header>\n' +
+        '            <Modal.Body>\n' +
+        '              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti ullam praesentium laudantium delectus...</p>\n' +
+        '              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure magni quia excepturi aliquid quas nostrum?</p>\n' +
+        '              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas nisi corrupti dolorem incidunt quisquam.</p>\n' +
+        '            </Modal.Body>\n' +
+        '            <Modal.Footer>\n' +
+        '              <Button primary size="large" onClick={() => setIsOpen(false)}>\n' +
+        '                Close\n' +
+        '              </Button>\n' +
+        '            </Modal.Footer>\n' +
+        '          </Modal.Content>\n' +
+        '        </>\n' +
+        '      )}\n' +
+        '    </Modal>',
+    },
+  },
+};
+
