56871b204fc2aa02cf0c2bffd6cc71164a9af5a4
Dmitrii Kliushentsev <dmitrii_kliushentsev@epam.com>
2022-01-18 15:36:23 +0300

    feat: add stories for system alert

--- src/components/system-alert/system-alert.stories.tsx
+++ src/components/system-alert/system-alert.stories.tsx
@@ -10,7 +10,16 @@ export default {
 } as Meta;
 
 const Action: FC = () => <SystemAlert.Action onClick={() => alert('Run Action')}>Action</SystemAlert.Action>;
-const Template:Story = (args) => (<SystemAlert title="Title" type="info" action={<Action />} {...args}>Title</SystemAlert>);
+const Template:Story = (args) => (<SystemAlert title="Title" type="info" action={<Action />} {...args}>Content</SystemAlert>);
 
 export const Info = Template.bind({});
 Info.args = { type: 'info', title: 'Title' };
+
+export const Error = Template.bind({});
+Error.args = { type: 'error', title: 'Title' };
+
+export const Warning = Template.bind({});
+Warning.args = { type: 'warning', title: 'Title' };
+
+export const Success = Template.bind({});
+Success.args = { type: 'success', title: 'Title' };
